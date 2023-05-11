6de9372bb3f59173e592a83fdd6f7d25f606d4db
MaxwellConway <maxconway23@gmail.com>
2023-05-10 22:17:13 -0400

    update
    possible this ruins everything

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-console.log(
-  fetch("https://eldenring.fanapis.com/api/ammos?limit=100", {
+//console.log(
+//  fetch("https://eldenring.fanapis.com/api/ammos?limit=100", {
+//    method: "GET",
+//  })
+//);
+//const searchButton = document.querySelector("#searchButton");
+
+//searchButton.addEventListener("click", () => getAmmos());
+
+async function getWeapons() {
+  const weapons = await fetch("https://eldenring.fanapis.com/api/weapons", {
     method: "GET",
-  })
-);
+  });
 
-const searchButton = document.querySelector("#searchButton");
-searchButton.addEventListener("click", () => getAmmos());
-
-async function getAmmos() {
-  const ammos = await fetch(
-    "https://eldenring.fanapis.com/api/ammos?limit=100",
-    {
-      method: "GET",
-    }
-  );
-
-  const json = await ammos.json();
-  const ammosArray = json.data;
-  console.log(ammosArray);
-  return ammosArray;
+  const json = await weapons.json();
+  const weaponsArray = json.data;
+  console.log(weaponsArray);
 }
 
-console.log(getAmmos());
+async function getSorceries() {
+  const sorceries = await fetch("https://eldenring.fanapis.com/api/sorceries", {
+    method: "GET",
+  });
+
+  const json = await sorceries.json();
+  const sorceriesArray = json.data;
+  console.log(sorceriesArray);
+}
+
+//const buildCard =
+//(ALL OF THE DATA THAT i WANT TO INCLUDE ON THE CARD AKA WEAPONS, SPELLS/INCANTS, PRIMARY STATS, STATUS EFFECTS/DAMAGE TYPES?, TALISMANS)
+//
